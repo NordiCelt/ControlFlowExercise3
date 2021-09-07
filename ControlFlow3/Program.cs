aec5224072d96f127f73de8c7904e8fdfbaa7250
NordiCelt <tribouletr@gmail.com>
2021-09-07 06:33:36 -0500

    Add project files.

---
+++ ControlFlow3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControlFlow3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I understand that you would like to submit a picture, but I need it's dimensions... What is the width of the picture in inches? ");
+            float widthInches = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Okay! What is the height of the picture in inches? ");
+            float heightInches = float.Parse(Console.ReadLine());
+
+            if (widthInches > heightInches)
+                Console.WriteLine("Your picture is landscape!");
+            else if (heightInches > widthInches)
+                Console.WriteLine("Your picture is portrait!");
+            else
+                Console.WriteLine("Your picture is a square!");
+        }
+    }
+}
